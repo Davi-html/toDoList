ea20bfc032ce3e828383376f03e59a4408acaeaf
davi <davifernandes3674@gmail.com>
2023-12-18 18:36:05 -0300

    criando a funcao de enciar a lista com a tecla 'enter'

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const ul = document.querySelector('ul')
-const enviar = document.querySelector('enviar')
+const enviar = document.querySelector('.enviar')
+const input = document.querySelector('input')
 
 function criarLi(texto){
+    let li = document.createElement('li')
+    li.innerHTML = texto
+    ul.appendChild(li)
     
 }
+
+enviar.addEventListener('click', (e)=>{
+    criarLi(input.value)
+})
+
+function enviarENTER(event){
+    let enter = event.keyCode
+    if(enter === 13){
+        criarLi(input.value)
+    }
+}
+
+input.addEventListener('keypress', enviarENTER)
